d19eb58b946314fd0f08876e3b642b90b8614a61
Bassey E. Otudor <bassey.otudor@outlook.com>
2025-03-26 03:50:35 +0100

    Minor changes: Moved TABLE_ID and REV_ID to the nested Constants class.

--- androidlibrary_lib/src/androidTest/java/org/opendatakit/database/data/TableMetaDataEntriesTest.java
+++ androidlibrary_lib/src/androidTest/java/org/opendatakit/database/data/TableMetaDataEntriesTest.java
@@ -13,39 +13,58 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class TableMetaDataEntriesTest {
 
-    //
     private TableMetaDataEntries testMetaDataEntries;
-    private final String TABLE_ID = "testTable";
-    private final String REV_ID = "revId";
+
     private final KeyValueStoreEntry ENTRY_1 = new KeyValueStoreEntry();
     private final KeyValueStoreEntry ENTRY_2 = new KeyValueStoreEntry();
 
+    // Contants class containing content to be used in tests
+    private static class Constants {
+
+        private static final String TABLE_ID = "testTable";
+        private static final String REV_ID = "revId";
+
+        // first test entry constents
+        private static final String PARTITION1 = "partition1";
+        private static final String ASPECT1 = "aspect1";
+        private static final String KEY1 = "key1";
+        private static final String TYPE1 = "number";
+        private static final String VALUE1 = "value1";
+
+        // second test entry contents
+        private static final String PARTITION2 = "partition2";
+        private static final String ASPECT2 = "aspect2";
+        private static final String KEY2 = "key2";
+        private static final String TYPE2 = "number";
+        private static final String VALUE2 = "value2";
+    }
+
     @Before
     public void setUp() {
-        testMetaDataEntries = new TableMetaDataEntries(TABLE_ID, REV_ID);
+        testMetaDataEntries = new TableMetaDataEntries(Constants.TABLE_ID, Constants.REV_ID);
 
         // first test entry
-        ENTRY_1.tableId = TABLE_ID;
-        ENTRY_1.partition = "partition1";
-        ENTRY_1.aspect = "aspect1";
-        ENTRY_1.key = "key1";
-        ENTRY_1.type = "number";
-        ENTRY_1.value = "value1";
+        ENTRY_1.tableId = Constants.TABLE_ID;
+        ENTRY_1.partition = Constants.PARTITION1;
+        ENTRY_1.aspect = Constants.ASPECT1;
+        ENTRY_1.key = Constants.KEY1;
+        ENTRY_1.type = Constants.TYPE1;
+        ENTRY_1.value = Constants.VALUE1;
 
         // second test entry
-        ENTRY_2.tableId = TABLE_ID;
-        ENTRY_2.partition = "partition2";
-        ENTRY_2.aspect = "aspect2";
-        ENTRY_2.key = "key2";
-        ENTRY_2.type = "number";
-        ENTRY_2.value = "value2";
+        ENTRY_2.tableId = Constants.TABLE_ID;
+        ENTRY_2.partition = Constants.PARTITION2;
+        ENTRY_2.aspect = Constants.ASPECT2;
+        ENTRY_2.key = Constants.KEY2;
+        ENTRY_2.type = Constants.TYPE2;
+        ENTRY_2.value = Constants.VALUE2;
 
     }
 
     @Test
     public void testDatabaseInitialization() { // test that an empty database is initialized
-        assertEquals(TABLE_ID, testMetaDataEntries.getTableId());
-        assertEquals(REV_ID, testMetaDataEntries.getRevId());
+        assertEquals(Constants.TABLE_ID, testMetaDataEntries.getTableId());
+        assertEquals(Constants.REV_ID, testMetaDataEntries.getRevId());
         assertTrue(testMetaDataEntries.getEntries().isEmpty());
     }
 
@@ -54,10 +73,28 @@ public class TableMetaDataEntriesTest {
         testMetaDataEntries.addEntry(ENTRY_1);
         testMetaDataEntries.addEntry(ENTRY_2);
 
+        // Verify that the entries were added correctly
         ArrayList<KeyValueStoreEntry> entries = testMetaDataEntries.getEntries();
         assertEquals(2, entries.size());
         assertEquals(ENTRY_1, entries.get(0));
         assertEquals(ENTRY_2, entries.get(1));
+
+        // Verify that the contents of the entries are correct
+        // ENTRY_1
+        assertEquals(Constants.TABLE_ID, entries.get(0).tableId);
+        assertEquals(Constants.PARTITION1, entries.get(0).partition);
+        assertEquals(Constants.ASPECT1, entries.get(0).aspect);
+        assertEquals(Constants.KEY1, entries.get(0).key);
+        assertEquals(Constants.TYPE1, entries.get(0).type);
+        assertEquals(Constants.VALUE1, entries.get(0).value);
+
+        // ENTRY_2
+        assertEquals(Constants.TABLE_ID, entries.get(1).tableId);
+        assertEquals(Constants.PARTITION2, entries.get(1).partition);
+        assertEquals(Constants.ASPECT2, entries.get(1).aspect);
+        assertEquals(Constants.KEY2, entries.get(1).key);
+        assertEquals(Constants.TYPE2, entries.get(1).type);
+        assertEquals(Constants.VALUE2, entries.get(1).value);
     }
 
     @Test
@@ -99,6 +136,5 @@ public class TableMetaDataEntriesTest {
         assertEquals(ENTRY_2, entries.get(0));
         assertEquals(ENTRY_1, entries.get(1));
     }
-
-
 }
+
